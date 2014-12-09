39173513632c1f1e30c0c7590621dee47cf8794e
Galen Emery <galen@galenemery.com>
2014-12-08 22:40:03 -0800

    Version bump metadata.rb, update rake task

--- Rakefile
+++ Rakefile
@@ -2,7 +2,7 @@ namespace :berkshelf do
 
   desc "Upload cookbook to chef server"
   task :upload do
-    sh "bundle exec berks upload -c deploy/berkshelf.json"
+    sh "bundle exec berks upload -c berkshelf.json"
   end
 
 end
--- VS-FourthCoffee/metadata.rb
+++ VS-FourthCoffee/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jdunn@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures the Fourth Coffee demonstration website'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.3'
 
 depends          'dsc'
 depends          'iis'
