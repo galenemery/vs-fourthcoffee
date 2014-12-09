a1248ff0db8038e71f4523f234eb110605e7f70e
Galen Emery <galen@galenemery.com>
2014-12-09 00:54:25 -0800

    Added .chefignore

--- VS-FourthCoffee/metadata.rb
+++ VS-FourthCoffee/metadata.rb
@@ -1,10 +1,10 @@
 name             'fourthcoffee'
 maintainer       'Chef Software, Inc.'
-maintainer_email 'jdunn@getchef.com'
+maintainer_email 'galen@getchef.com'
 license          'Apache 2.0'
 description      'Installs and configures the Fourth Coffee demonstration website'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends          'dsc'
 depends          'iis'
